6a1a63a9c81c396f960723aa747a6dcc1640d0a2
ewa1do <this.eduardovera@gmail.com>
2022-03-21 21:10:47 -0400

    Lesson 2: Middlewares

--- server.js
+++ server.js
@@ -4,27 +4,37 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('getting root');
+// Lesson 2: Middlewares
+
+app.use((req, res, next) => {
+    console.log('<h1>Hello</h1>');
+    next();
 });
 
-app.get('/profile', (req, res) => {
-    res.send('getting profile');
+app.get ('/', (req, res) => {
+    res.send('test');
 });
 
-app.post('/profile', (req, res) => {
-    const user = {
-        name: 'Sally',
-        hobby: 'Soccer'
-    }
-    res.send(user);
-});
+
+// // Lesson 1
+// app.get('/', (req, res) => {
+//     res.send('getting root');
+// });
+
+// app.get('/profile', (req, res) => {
+//     res.send('getting profile');
+// });
+
+// app.post('/profile', (req, res) => {
+//     const user = {
+//         name: 'Sally',
+//         hobby: 'Soccer'
+//     }
+//     res.send(user);
+// });
 
 
 const PORT = 5000;
-
-
-
 
 app.listen(PORT, (req, res) => {
     console.log('Conecting to server on port:'+PORT);
